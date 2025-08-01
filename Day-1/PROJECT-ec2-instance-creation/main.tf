0073b99db350c208b7eb0242d85447cbd224b8c5
VanshaCloud2605 <vanshakhernhce@gmail.com>
2025-08-01 06:43:08 +0000

    Added correct AMI ID, inst-type and keyname

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,7 +2,10 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws_instance" "VM_instance" {
+    ami           = "ami-020cba7c55df1f615"   # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id     = "subnet-0afb91067858e0d84"
+    key_name = "18feb_key"
 }
+
